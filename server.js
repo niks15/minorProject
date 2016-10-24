b89338fb6554323989c26fd71846d68ac7a8fc2d
Nikhil Singh <Nikhil Singh>
2016-10-24 16:58:57 +0530

    adding files

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+var mongojs = require('mongojs');
+var db = mongojs('contactList', ['contactList']);
+
+var bodyParser = require('body-parser');
+
+
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+
+
+app.listen(3000);
+
+console.log("server running on port 3000");
